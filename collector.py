61dc5fb0724c60378594ee6e202db022ce64c39c
chris <chris@exosphere.de>
2013-01-16 22:26:47 +0100

    modularize main script

--- collector.py
+++ collector.py
@@ -13,16 +13,27 @@ import spur
 import ConfigParser
 import datetime
 
-config = ConfigParser.SafeConfigParser()
-config.read('dooris.cfg')
+CONFIG = ConfigParser.SafeConfigParser()
+CONFIG.read('dooris.cfg')
 
-shell = spur.SshShell(hostname=config.get('door', 'sshhost'),
-                      username=config.get('door', 'sshuser'),
-                      private_key_file=config.get('door', 'sshkey'))
-doorstatus = shell.run(['cat', '/sys/class/gpio/gpio0/value'])
+def getdoorstatus():
+    """
+    Read door status via SSH to Raspi.
+    """
+    shell = spur.SshShell(hostname=CONFIG.get('door', 'sshhost'),
+                        username=CONFIG.get('door', 'sshuser'),
+                        private_key_file=CONFIG.get('door', 'sshkey'))
+    doorstatus = shell.run(['cat', '/sys/class/gpio/gpio0/value'])
+    print 'door is closed' if doorstatus.output else 'door is open'
+    return doorstatus.output
 
-print 'door is closed' if doorstatus.output else 'door is open'
+def writeoutput(doorstatus):
+    """
+    Write output files.
+    """
+    with open(CONFIG.get('general', 'classicoutputfile'), 'w') as cof:
+        cof.write(doorstatus.output)
+        cof.write('{}\n'.format(datetime.datetime.now().strftime('%s')))
 
-with open(config.get('general', 'classicoutputfile'), 'w') as f:
-    f.write(doorstatus.output)
-    f.write('{}\n'.format(datetime.datetime.now().strftime('%s')))
+if __name__ == "__main__":
+    writeoutput(getdoorstatus())
