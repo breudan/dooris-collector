407ecaf251afdd04c6823485975971f652d5196d
chris <chris@exosphere.de>
2013-01-17 17:59:47 +0100

    json api version + pep8.

--- collector.py
+++ collector.py
@@ -22,8 +22,8 @@ CONFIG.read('dooris.cfg')
 def fetch_doorstatus():
     """Read door status via SSH to Raspi."""
     shell = spur.SshShell(hostname=CONFIG.get('door', 'sshhost'),
-                        username=CONFIG.get('door', 'sshuser'),
-                        private_key_file=CONFIG.get('door', 'sshkey'))
+                          username=CONFIG.get('door', 'sshuser'),
+                          private_key_file=CONFIG.get('door', 'sshkey'))
     doorstatus = shell.run(['cat', '/sys/class/gpio/gpio0/value'])
     # TODO error handling
     return doorstatus.output.strip()
@@ -31,10 +31,10 @@ def fetch_doorstatus():
 
 def write_output():
     """Collect data and write output files."""
-    result = {'door': { 'status': fetch_doorstatus(),
-                        'last_change': 0, # TODO calculate last change
-                        'last_update': datetime.datetime.now().strftime('%s')}
-             }
+    result = {'door': {'status': fetch_doorstatus(),
+                       'last_change': 0,  # TODO calculate last change
+                       'last_update': datetime.datetime.now().strftime('%s')},
+              'apiversion': '0.1'}
 
     with open(CONFIG.get('general', 'jsonoutputfile'), 'w') as jof:
         jof.write('{0}\n'.format(json.dumps(result)))
