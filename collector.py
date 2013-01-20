f9b90aa2d8cf7a7fb4194f706e7ce2107b7c08d9
chris <chris@exosphere.de>
2013-01-20 20:42:31 +0100

    refactor shared code into methods.

--- collector.py
+++ collector.py
@@ -40,50 +40,44 @@ class Collector:
         except IOError:
             self.output = {'apiversion': self.apiversion}
 
-    def fetch_doorstatus(self):
-        """Read door status via SSH to Raspi."""
-        shell = spur.SshShell(hostname=self.config.get('door', 'sshhost'),
-                              username=self.config.get('door', 'sshuser'),
-                              port=int(self.config.get('door', 'sshport')),
-                              private_key_file=self.config.get('door',
+    def _ssh_exec(self, module, command):
+        """Read config for module, execute command via SSH."""
+        shell = spur.SshShell(hostname=self.config.get(module, 'sshhost'),
+                              username=self.config.get(module, 'sshuser'),
+                              port=int(self.config.get(module, 'sshport')),
+                              private_key_file=self.config.get(module,
                                                                'sshkey'))
         try:
-            doorstatus = shell.run(['cat',
-                                    '/sys/class/gpio/gpio0/value']).output
-            doorstatus = doorstatus.strip()
+            result = shell.run(command).output
         except:
             # no difference in misconfiguration and actual errors.
             # might need improvement. :)
-            doorstatus = '-1'
+            result = '-1'
+        return result
+
+    def _update_result(self, module, value):
+        """update output with new value for module"""
         now = int(datetime.datetime.now().strftime('%s'))
-        if not 'door' in self.output:
-            self.output['door'] = {'status': '-1'}
-        if self.output['door']['status'] != doorstatus:
-            self.output['door']['status'] = doorstatus
-            self.output['door']['last_change'] = now
-        self.output['door']['last_update'] = now
+        if not module in self.output:
+            self.output[module] = {value[0]: '-1'}
+        if self.output[module][value[0]] != value[1]:
+            self.output[module][value[0]] = value[1]
+            self.output[module]['last_change'] = now
+        self.output[module]['last_update'] = now
+
+    def fetch_doorstatus(self):
+        """Read door status via SSH to Raspi."""
+        doorstatus = self._ssh_exec('door', ['cat',
+                                             '/sys/class/gpio/gpio0/value'])
+        doorstatus = doorstatus.strip()
+        self._update_result('door', ('status', doorstatus))
 
     def fetch_routerstatus(self):
         """Read router status via SSH to router."""
-        shell = spur.SshShell(hostname=self.config.get('router', 'sshhost'),
-                              username=self.config.get('router', 'sshuser'),
-                              port=int(self.config.get('router', 'sshport')),
-                              private_key_file=self.config.get('router',
-                                                               'sshkey'))
-        try:
-            dhcpclients = shell.run(['wc', '-l', '/tmp/dhcp.leases']).output
-            dhcpclients = dhcpclients.split()[0].strip()
-        except:
-            # no difference in misconfiguration and actual errors.
-            # might need improvement. :)
-            dhcpclients = '-1'
-        now = int(datetime.datetime.now().strftime('%s'))
-        if not 'router' in self.output:
-            self.output['router'] = {'dhcp': '-1'}
-        if self.output['router']['dhcp'] != dhcpclients:
-            self.output['router']['dhcp'] = dhcpclients
-            self.output['router']['last_change'] = now
-        self.output['router']['last_update'] = now
+        dhcpclients = self._ssh_exec('router', ['wc', '-l',
+                                                '/tmp/dhcp.leases'])
+        dhcpclients = dhcpclients.split()[0].strip()
+        self._update_result('router', ('dhcp', dhcpclients))
 
     def write_output(self):
         """Collect data and write output files."""
